b3d5c4c6ca87812a131dc80bfe257c890f15999d
user <esy_win@hotmail.com>
2022-03-19 20:31:25 +0100

    infosec.sh goes testing

--- infosec.sh
+++ infosec.sh
@@ -5,39 +5,44 @@
 # prompt the user for username
 echo 'Enter your desired username :'
 # username prompt
-read username
+read _username
+# prompt the user for hostname
+echo 'Enter your desired hostname :'
+# username prompt
+read _hostname
 # prompt the user for password
 echo 'Enter your desired password :'
 # password prompt
-read password
+read _password
 # create user from prompt
-useradd -m $username
+useradd -m $_username
 # assign password from prompt to passwd $username
 {
-    echo $password;
-    echo $password;
-} | passwd $username
+    echo $_password;
+    echo $_password;
+} | passwd $_username
 # create admin group
 groupadd admin
 # add username from prompt to admin group
-usermod -aG admin $username
+usermod -aG admin $_username
+usermod -aG sudo $_username
 
 ### SSH CONFIG ###
 
 # prompt
 echo 'Enter your desired port to use SSH :'
 read ssh_port
-echo 'Type these commands from your desktop linux/mac to create & add yourself to SSH trusted users :'
-echo '  ssh-keygen -b 4096 -t rsa'
-echo '  ssh-copy-id -i ~/.ssh/id_rsa.pub username@remotePublicIPAddress'
+# echo 'Type these commands from your desktop linux/mac to create & add yourself to SSH trusted users :'
+# echo '  ssh-keygen -b 4096 -t rsa'
+# echo '  ssh-copy-id -i ~/.ssh/id_rsa.pub username@remotePublicIPAddress'
 # change ssh port
 sed -i 's/#Port 22/Port $ssh_port/g' /etc/ssh/sshd_config
 # disable root login
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 # disable password auth
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+# sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 # allow pubkey auth
-sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+# sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 # test config
 sshd -t
 # allow new ssh port
@@ -45,7 +50,7 @@ ufw allow $ssh_port
 # restart ssh deamon
 systemctl restart sshd
 # notify user how to connect through SSH next time
-echo 'use "ssh '$username@remotePublicIPAddress' -p $ssh_port" to connect to your VPS next time'
+# echo 'use "ssh '${_username}@remotePublicIPAddress' -p $ssh_port" to connect to your VPS next time'
 
 ### FAIL2BAN CONFIG ###
 
